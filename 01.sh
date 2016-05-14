a3758d36f0fad39332371b5402ed69bb26f9eb22
jerryyu <dengfeng2012@163.com>
2016-05-14 21:23:54 +0800

    add echo -n

--- 01.sh
+++ 01.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 # This script displays the date and who's logged on  
-echo The time and date are:
+echo -n The time and date are: #-n 可使得echo命令结果和echo文本字符串在同一行
 date
 echo "Let's see who logged on the system:"
 who
